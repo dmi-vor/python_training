21f2d8be17a8f4b5800a6063c72a6cae750a71fa
dmi-vor <janeon@bk.ru>
2021-03-27 22:51:44 +0200

    Выделен класс для управления сессиями.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,7 +12,7 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="Степан", middlename="Дядя Степа", lastname="Степанов", nickname="Каланча", title="Бывший флотский старшина", company="Застава Ильича", address="Дом 8/1 у заставы Ильича", home="+7001001001", mobile="+7002002002",
                             work="+7003003003", fax="+7004004004", email_1="e.email1@mail.ma", email_2="e.email2@mail.ma", email_3="e.email3@mail.ma", homepage="https://www.culture.ru/poems/45240/dyadya-stepa", address_2="address", home_2="home", notes="Знают взрослые и дети, Весь читающий народ, Что, живя на белом свете, Дядя Стёпа не умрёт!"))
-    app.logout()
+    app.session.logout()
