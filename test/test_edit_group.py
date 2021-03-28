0c6e7fba7caa65362019165b20703feabf1748f6
dmi-vor <janeon@bk.ru>
2021-03-28 15:17:39 +0300

    Оптимизация ввода данных в формы.

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,7 +2,13 @@
 from model.group import Group
 
 
-def test_edit_group(app):
+#def test_edit_group(app):
+#        app.session.login(username="admin", password="secret")
+#        app.group.edit_first_group(Group(name="modA", header="modB", footer="modC"))
+#        app.session.logout()
+
+
+def test_edit_group_name(app):
         app.session.login(username="admin", password="secret")
-        app.group.edit(Group(name="modA", header="modB", footer="modC"))
+        app.group.edit_first_group(Group(name="modA"))
         app.session.logout()
