e3af327480e0a52e93735955d879e60cc8d5717e
dmi-vor <janeon@bk.ru>
2021-03-28 19:00:03 +0300

    Задание №10: оптимизированы переходы между страницами

--- fixture/contact.py
+++ fixture/contact.py
@@ -57,7 +57,8 @@ class ContactHelper:
 
     def goto_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_xpath("//input[@value='Delete']")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def count(self):
         wd = self.app.wd
