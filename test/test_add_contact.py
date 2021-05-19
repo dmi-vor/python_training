5a5644dc210b65626201e8fa28a1462f678d0309
dmi-vor <janeon@bk.ru>
2021-05-20 00:16:13 +0300

    Задание №18-19: Реализованы генераторы тестовых данных и загрузка тестовых данных из файлов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,14 +2,14 @@
 from model.contact import Contact
 
 
-def test_add_contact_name(app):
-    old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Степан", lastname="Stepanoff")
-    app.contact.create(contact)
-    new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+#def test_add_contact_name(app):
+#    old_contacts = app.contact.get_contact_list()
+#    contact = Contact(firstname="Степан", lastname="Stepanoff")
+#    app.contact.create(contact)
+#    new_contacts = app.contact.get_contact_list()
+#    assert len(old_contacts) + 1 == len(new_contacts)
+#    old_contacts.append(contact)
+#    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_add_contact(app, json_contacts):
